9605d69e339cb748a3be4dbde3d6f04970395c6f
ulbertAO <sp595993@gmail.com>
2025-08-23 15:54:51 +0530

    removed default code and added department controller

---
+++ OrgManager/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrgManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private static readonly string[] Departments = new[]
+        { "Engineering", "Data Science", "Human Resource"};
+        private readonly ILogger<DepartmentController> _logger;
+
+        public DepartmentController(ILogger<DepartmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Departments);
+        }
+    }
+}
